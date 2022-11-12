587707193583c1b9891b11ad810d546869086925
PrashanthaPujari <prashanthv881@gmail.com>
2022-11-12 08:47:06 +0530

    us o1 added

--- src/main/java/pom/KhanacademyLoginpage.java
+++ src/main/java/pom/KhanacademyLoginpage.java
@@ -1,5 +1,10 @@
 package pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class KhanacademyLoginpage {
+	
+	@FindBy () private WebElement xyz;
 
 }
